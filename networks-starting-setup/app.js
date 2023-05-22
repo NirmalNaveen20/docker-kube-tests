e85b41685506aceec98a99559fff0cb6758f86c2
NirmalNaveen20 <nirmalnaveen95@gmail.com>
2023-05-22 14:33:09 +0530

    update app.js

--- networks-starting-setup/app.js
+++ networks-starting-setup/app.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios').default;
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios").default;
+const mongoose = require("mongoose");
 
-const Favorite = require('./models/favorite');
+const Favorite = require("./models/favorite");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/favorites', async (req, res) => {
+app.get("/favorites", async (req, res) => {
   const favorites = await Favorite.find();
   res.status(200).json({
     favorites: favorites,
   });
 });
 
-app.post('/favorites', async (req, res) => {
+app.post("/favorites", async (req, res) => {
   const favName = req.body.name;
   const favType = req.body.type;
   const favUrl = req.body.url;
 
   try {
-    if (favType !== 'movie' && favType !== 'character') {
+    if (favType !== "movie" && favType !== "character") {
       throw new Error('"type" should be "movie" or "character"!');
     }
     const existingFav = await Favorite.findOne({ name: favName });
     if (existingFav) {
-      throw new Error('Favorite exists already!');
+      throw new Error("Favorite exists already!");
     }
   } catch (error) {
     return res.status(500).json({ message: error.message });
@@ -43,32 +43,32 @@ app.post('/favorites', async (req, res) => {
     await favorite.save();
     res
       .status(201)
-      .json({ message: 'Favorite saved!', favorite: favorite.toObject() });
+      .json({ message: "Favorite saved!", favorite: favorite.toObject() });
   } catch (error) {
-    res.status(500).json({ message: 'Something went wrong.' });
+    res.status(500).json({ message: "Something went wrong." });
   }
 });
 
-app.get('/movies', async (req, res) => {
+app.get("/movies", async (req, res) => {
   try {
-    const response = await axios.get('https://swapi.dev/api/films');
+    const response = await axios.get("https://swapi.dev/api/films");
     res.status(200).json({ movies: response.data });
   } catch (error) {
-    res.status(500).json({ message: 'Something went wrong.' });
+    res.status(500).json({ message: "Something went wrong." });
   }
 });
 
-app.get('/people', async (req, res) => {
+app.get("/people", async (req, res) => {
   try {
-    const response = await axios.get('https://swapi.dev/api/people');
+    const response = await axios.get("https://swapi.dev/api/people");
     res.status(200).json({ people: response.data });
   } catch (error) {
-    res.status(500).json({ message: 'Something went wrong.' });
+    res.status(500).json({ message: "Something went wrong." });
   }
 });
 
 mongoose.connect(
-  'mongodb://localhost:27017/swfavorites',
+  "mongodb://mongodb-app:27017/swfavorites",
   { useNewUrlParser: true, useUnifiedTopology: true }, // Add the useUnifiedTopology option
   (err) => {
     if (err) {
@@ -78,4 +78,3 @@ mongoose.connect(
     }
   }
 );
-
