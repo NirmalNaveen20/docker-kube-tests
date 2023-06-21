35e62cad247e465fadaed1ba5afe519ea857a925
NirmalNaveen20 <nirmalnaveen95@gmail.com>
2023-06-21 11:52:11 +0530

    Add .dockerignore

--- data-volumes-01-starting-setup/server.js
+++ data-volumes-01-starting-setup/server.js
@@ -36,10 +36,11 @@ app.post('/create', async (req, res) => {
     if (exists) {
       res.redirect('/exists');
     } else {
-      await fs.rename(tempFilePath, finalFilePath);
+      await fs.copyFile(tempFilePath, finalFilePath);
+      await fs.unlink(tempFilePath);
       res.redirect('/');
     }
   });
 });
 
-app.listen(80);
+app.listen(process.env.PORT);
